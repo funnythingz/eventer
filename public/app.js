0f78b769d1abf6bb666c4ca82d80ef1e18284846
funnythingz <hiroki@hackleberry.me>
2013-11-27 18:30:54 +0900

    Datepicker test

--- public/app.js
+++ public/app.js
@@ -20,3 +20,43 @@ eventer.controller('addEventCtrl', function ($scope) {
         $scope.candidateDates.push({ value: '' });
     };
 });
+;var DatepickerDemoCtrl = function ($scope, $timeout) {
+    $scope.today = function () {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.showWeeks = true;
+    $scope.toggleWeeks = function () {
+        $scope.showWeeks = !$scope.showWeeks;
+    };
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    $scope.disabled = function (date, mode) {
+        return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+    };
+
+    $scope.toggleMin = function () {
+        $scope.minDate = ($scope.minDate) ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function () {
+        console.log(0);
+        $timeout(function () {
+            console.log(1);
+            $scope.opened = true;
+        });
+    };
+
+    $scope.dateOptions = {
+        'year-format': "'yy'",
+        'starting-day': 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+    $scope.format = $scope.formats[0];
+};
